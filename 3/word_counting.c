ca5c0f21616027dc28080842c777f25a172ac89f
deepabalan <deepa.deepabalan@gmail.com>
2017-04-16 22:52:04 +0530

    added word counting problem

---
+++ 3/word_counting.c
@@ -0,0 +1,26 @@
+// Simple program that counts each word.
+
+
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+main()
+{
+	int c, nw, nl, nc, state;
+	nw = nl = nc = 0;
+	state = OUT;
+
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+		}
+		else if (state == OUT) {
+			state = IN;
+			++nw;
+		}
+	}
+	printf("word count = %d, line count = %d, charcater coutn = %d\n", nw, nl, nc);
+}
