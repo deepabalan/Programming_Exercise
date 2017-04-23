74a31a44f2da81a4f5589187d36be665d60e3190
deepabalan <deepa.deepabalan@gmail.com>
2017-04-23 13:42:06 +0530

    pushed 2 problems

---
+++ 3/blank_replace_rpt.c
@@ -0,0 +1,17 @@
+// replace multiple blanks with single blank
+
+
+#include <stdio.h>
+#define NONBLANK 'a'
+
+main()
+{
+	int c, lastc;
+	lastc = NONBLANK;
+
+	while ((c = getchar()) != EOF) {
+		if (c != ' ' || lastc != c)
+			putchar(c);
+		lastc = c;
+	}
+}
